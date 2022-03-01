fcd277d41c02ca1fe28a120e056174b12cb1c2df
{kaplunovsky} <{kaplunovski@mail.ru}>
2022-03-01 18:38:55 +0300

    Init Commit

---
+++ src/test/java/RunnerTest.java
@@ -0,0 +1,15 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"stepDefinition"},
+        tags = "@Smoke"
+)
+
+
+public class RunnerTest {
+
+}
---
+++ src/test/java/stepDefenition/steps.java
@@ -0,0 +1,48 @@
+package stepDefenition;
+
+import io.cucumber.java.ru.Дано;
+import io.cucumber.java.ru.И;
+import io.cucumber.java.ru.Тогда;
+
+public class steps {
+    int a;
+    int b;
+    int sum;
+    int mines;
+    int pr;
+    int del;
+
+    @Дано("^два числа (.*) and (.*)$")
+    public void given(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    @Тогда("^находим сумму двух чисел$")
+    public void summa(){
+        sum = a + b;
+    }
+
+    @И("^находим разность двух чисел$")
+    public void minus(){
+        mines = a - b;
+    }
+
+    @И("^находим произведение двух чисел$")
+    public void proizv(){
+        pr = a * b;
+    }
+
+    @И("^находим деление двух чисел$")
+    public void delenie(){
+        del = a / b;
+    }
+
+    @И("^выводим сумму на экран$")
+    public void printResult(){
+        System.out.println("Сумма чисел " + a + " и " + b + " равняется " + sum);
+        System.out.println("Разница чисел " + a + " и " + b + " равняется " + mines);
+        System.out.println("Произведение чисел " + a + " и " + b + " равняется " + pr);
+        System.out.println("Деление чисел " + a + " и " + b + " равняется " + del);
+    }
+}
